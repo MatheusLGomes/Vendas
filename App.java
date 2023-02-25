0f0bd31a134e894b94e580518e4e73e222236e41
MatheusLGomes <111156565+MatheusLGomes@users.noreply.github.com>
2023-02-24 23:38:24 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // 1° instaciando o objeto
+        
+        Scanner input = new  Scanner(System.in);
         Vendedor vendedor = new Vendedor();
         // Declarando valores aos atributos
-        vendedor.setNome("Matheus");
-        vendedor.setSalario(1200);
-        vendedor.setVendas(800);
-        vendedor.setComissão(0.15);
-        double comissão = (vendedor.getVendas() * vendedor.getComissão()) + vendedor.getSalario();
-        vendedor.calcularComissão(vendedor.getComissão(), vendedor.getVendas());
-        System.out.println("Olá "+ vendedor.getNome()+ "! Seu salário fixo é "+ vendedor.getSalario()+ " e o seu salário do mês é: "+ vendedor.calcularComissão(comissão, comissão));
+        System.out.println("Digite o seu nome: ");
+        vendedor.setNome(input.next());
+        System.out.println("Digite o seu salário : ");
+        vendedor.setSalario(input.nextDouble());
+        System.out.println("Digite as suas vendas: ");
+        vendedor.setVendas(input.nextDouble());
+        
+        vendedor.calcularComissão();
+        vendedor. calcularSalario_Final();
+
+        System.out.println("Seus dados são: ");
+		System.out.println("Nome: " + vendedor.getNome() + " Salário: " + vendedor.getSalario() + " Salário final:" + vendedor.getSalario_final()) ;
+		
     }
 }
 
