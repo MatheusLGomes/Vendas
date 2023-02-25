4dd8c50062ac16d1a15e110ea14213e0df70a8a2
MatheusLGomes <111156565+MatheusLGomes@users.noreply.github.com>
2023-02-24 23:38:47 -0300

    Update Vendedor.java

--- Vendedor.java
+++ Vendedor.java
@@ -2,21 +2,21 @@ public class Vendedor {
     private String nome;
     private double salario_fixo;
     private double vendas;
-    public double comissão;
- 
+    private double comissão;
+    private double salario_final;
+    
+   
     //construtor
-    public Vendedor (String nome, double salario_fixo, double vendas, double comissão) {
+    public Vendedor (String nome, double salario_fixo, double vendas) {
      this.nome = nome;
      this.salario_fixo = salario_fixo;
      this.vendas = vendas;
-     this.comissão = comissão;
+  
     }
     public Vendedor(){
  
     }
-    public double calcularComissão( double vendas, double comissão){
-     return (comissão * vendas);
-    }
+ 
     
     public String getNome(){
      return nome;
@@ -41,12 +41,23 @@ public class Vendedor {
      this.vendas = vendas;
     }
  
+     
+     public double calcularComissão(){
+      return comissão = this.vendas*0.15;
+    
+     }
+
+     public double calcularSalario_Final(){
+      return salario_final = comissão + salario_fixo;
+     }
+
+     public double getSalario_final(){
+      return salario_final;
+     }
+
      public double getComissão(){
-     return comissão;
-    }
-    public void setComissão(double comissão){
-     this.comissão = comissão;
-    }
+      return comissão;
+     }
  }
  
  
